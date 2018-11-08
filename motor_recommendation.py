8801cbdade520f7099523573fcbaf05a80d5966a
voffer10 <32167479+voffer10@users.noreply.github.com>
2018-11-08 11:50:41 +0100

    Added description of MS

--- motor_recommendation.py
+++ motor_recommendation.py
@@ -1,5 +1,12 @@
 class Motor_recommendation():
     def __init__(self, weight, recommendation, hold_flag):
         self.weight = weight                                                #The weight of the MR
-        self.recommendation = recommendation                                #The recommendation
         self.hold_flag = hold_flag                                          #Hold flag
+        self.recommendation = recommendation                                # The recommendation
+        """
+        ("S", x) = Rotate a little to the to either left or right, x > 0 rotate right, x < 0 rotate left
+        ("L", x) = Turn left with difference x between the wheels
+        ("R", x) = Turn left with difference x between the wheels
+        ("F", x) = Drive straight forward with speed x
+        ("B", x) = Drive straight backward with speed x
+        """
