ddab5918b2301411ee2a1d8ebc1cc0c6d3b0f254
dashman-jr <31890256+dashman-jr@users.noreply.github.com>
2018-11-08 10:45:58 +0100

    Add files via upload

---
+++ behavior.py
@@ -0,0 +1,51 @@
+
+
+class Behavior:
+
+    def __init__(self, bbcon, priority):
+        self.bbcon = bbcon
+        self.sensobs = []  # Set of sensobs that the Behavior object updates based on
+        self.motor_recommendation = None  # MR object
+        self.active_flag = False  # Indicates whether Behavior is active or not
+        self.halt_request = False  # Used to request motors to stop when true
+        self.priority = priority  # Given constant indicating how much this Behavior-object should be prioritized
+        self.match_degree = 0.0  # Float in the range [0, 1] indicating how much the situation "matches" the behavior
+        self.weight = 0.0  # priority * match_degree
+
+    def add_sensob(self, sensob):  # Adds sensob, as long as it is not already in the list
+        if sensob not in self.sensobs:
+            self.sensobs.append(sensob)
+            # Add this Behavior-object to the sensob's list, so that both points to each other
+            sensob.add_behavior(self)
+
+    # Sets active_flag to False given certain conditions (OPTIONAL to define)
+    def consider_deactivation(self):
+        pass
+
+    # Sets active_flag to True given certain conditions (OPTIONAL to define)
+    def consider_activation(self):
+        pass
+
+    # Updates all the values of the Sensobs that the Behavior points to
+    # NOTE: DO NOT REDEFINE update()-method in subclasses
+    def update(self):
+        # Performs tests first to see whether object should (de)activate or not
+        if self.active_flag:
+            self.consider_deactivation()
+        else:
+            self.consider_activation()
+        # And then performs updates if Behavior is active
+        if self.active_flag and len(self.sensobs) != 0:
+            self.sense_and_act()
+            # match_degree must be calculated by sense_and_act()
+            if not (0.0 <= self.match_degree <= 1.0):
+                raise ValueError("Match_degree must be a float between 0 and 1, was ",
+                                 self.match_degree)
+            self.weight = self.priority * self.match_degree
+
+    # Performs the actual computations based on Sensob-values to produce
+    # motor_recommendations (MRs), potential halt request and match_degree
+    def sense_and_act(self):
+        print("behavior value: ", self.sensobs[0].value)
+        # raise NotImplementedError
+
---
+++ sensob.py
@@ -0,0 +1,58 @@
+
+
+class Sensob:
+
+    # Parameter sensors is a list of sensor objects,
+    # behaviors a set of Behavior objects
+    def __init__(self, sensors):
+        self.sensors = sensors  # List of sensors that the Sensob-object operates on
+        self.behaviors = []  # List of behaviors (no duplicates) that use the Sensob-object
+        self.active_flag = True  # Used by BBCON to only update sensors when their sensor objects are active
+        self.sensor_values = [None] * len(sensors)  # List of raw values collected from the sensors
+        self.value = None  # The preprocessed value that Behavior-objects will use
+
+    def add_behavior(self, behavior):  # Adds behavior, as long as it is not already in the list
+        if behavior not in self.behaviors:
+            self.behaviors.append(behavior)
+            # Add this Sensob-object to the behavior's list, so that both points to each other
+            behavior.add_sensob(self)
+
+    # This is the method that calculates the value used by Behavior-objects,
+    # will be overwritten in subclasses
+    def update(self):
+        # Sets flag false so Sensob-object will stay inactive if no behaviors activates it
+        self.active_flag = False
+        for behavior in self.behaviors:
+            if behavior.active_flag:
+                self.active_flag = True
+        if self.active_flag:
+            self.update_values()
+        return self.active_flag
+        # NOTE: Subclasses must call superclass update()-method
+        # before they process data and update self.value
+        # FORMAT OF update() IN SUBCLASS:
+        # def update(self):
+        #     if super(MySensobSubClass, self).update():
+        #         ...
+        #         *Your code here*
+        #         ...
+
+    def get_sensor_values(self):
+        return self.sensor_values
+
+    # Returns the computed value of the Sensob
+    def get_value(self):
+        return self.value
+
+    # Help method for updating and retrieving values from each sensor
+    def update_values(self):
+        for i in range(0, len(self.sensors)):
+            self.sensors[i].update()
+            self.sensor_values[i] = self.sensors[i].get_value()
+
+    # Method for resetting values
+    def reset(self):
+        for i in range(0, len(self.sensors)):
+            self.sensors[i].reset()
+            self.sensor_values[i] = None
+        self.value = None
