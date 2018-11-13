68f05a55e72fca3c13529465021bff533ccb7024
Magnus Reier Rushfeldt <magnurr@stud.ntnu.no>
2018-11-13 15:57:09 +0100

    Ultrasonic testing - init commit

--- avoid_obstacle_behavior.py
+++ avoid_obstacle_behavior.py
@@ -14,26 +14,30 @@ class AvoidObstacleBehavior(Behavior):
 
     # Sets active_flag to False given certain conditions (OPTIONAL to define)
     def consider_deactivation(self):
-        if self.sensobs[0].value > 10:
+        if self.sensobs[0].value > 20:
             self.active_flag = False
+            print("deactivated!")
             self.bbcon.deactivate_behaviour(self)
 
     # Sets active_flag to True given certain conditions (OPTIONAL to define)
     def consider_activation(self):
-        if self.sensobs[0].value <= 10:
+        if self.sensobs[0].value <= 20:
             self.active_flag = True
+            print("activated!")
             self.bbcon.activate_behaviour(self)
 
     # Performs the actual computations based on Sensob-values to produce
     # motor_recommendations (MRs), potential halt request and match_degree
     def sense_and_act(self):
         print("behavior value: ", self.sensobs[0].value)
-        self.match_degree = 5 / self.sensobs[0].value
+        self.match_degree = 5 / (self.sensobs[0].value + 0.01)
         if self.match_degree > 1:
             self.match_degree = 1
         if self.active_flag and self.sensobs[0].value < 5.2:
-            motor_recommendation = ('B',300)
+            motor_recommendation = ('B',0.3)
         else:
-            motor_recommendation = ('F',300)
-
-        self.motor_recommendation.update(self.priority * self.match_degree,motor_recommendation)
+            motor_recommendation = ('F',0.3)
+        #if self.sensobs[0].value < 1:
+        #    self.motor_recommendation.halt_request = True
+        self.motor_recommendation.update(self.priority * self.match_degree,motor_recommendation,
+                                         self.motor_recommendation.halt_request)
