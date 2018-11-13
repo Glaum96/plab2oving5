ed2543a272d822b67d18b10673b4856f28bc1a40
Kristoffer Håkonsen <kristoffer@hakonsen.com>
2018-11-13 13:23:45 +0100

    Small changes

--- motob.py
+++ motob.py
@@ -40,9 +40,9 @@ class Motob:
             # Stop code
         elif self.command == "S":              #What about timing?
             if self.degree < 0:
-                self.motors.set_value([-0.2, 0.2],1)
+                self.motors.set_value([-0.5, 0.5])
             else:
-                self.motors.set_value([0.2, -0.2],1)
+                self.motors.set_value([0.5, -0.5])
         elif self.command == "L":
             self.motors.left(self.degree)
         elif self.command =="R":
