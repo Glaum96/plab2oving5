d471ecf1b1f628ad22a53c9c31ce1ce263b1af85
Haakon Gunleiksrud <haakongunleiksrud@gmail.com>
2018-11-13 11:07:01 +0100

    Update findAndFollowRedBallBehavior.py

--- findAndFollowRedBallBehavior.py
+++ findAndFollowRedBallBehavior.py
@@ -6,27 +6,35 @@ from behavior import Behavior
 from bbcon import BBCON
 
 class FindAndFollowRedBallBehaviour(Behavior):
-    def __init__(self):
-        myBBCON = BBCON()
+
+    def __init__(self,myBBCON,priority):
         myFindRedSensob = FindRedSensob()
         sensobs = [myFindRedSensob]
-        priority = 1
-        Behavior.__init__(myBBCON,sensobs,priority)
+        Behavior.__init__(self,myBBCON,priority)
+        self.red_list = self.sensobs[0].get_value()
 
     def consider_deactivation(self):
         self.sensobs[0].update()
-        if max(self.sensobs[0].get_value() < 500):
-            self.sensobs[0].update()
+        if max(self.red_list) < 300:
             self.active_flag = False
             self.bbcon.deactivate_behaviour(self)
 
 
     def consider_activation(self):
-        if max(self.red_list >= 500):
+        if max(self.red_list >= 300):
             self.active_flag = True
             self.bbcon.activate_behaviour(self)
 
     def sense_and_act(self):
         print("behavior value: ", self.sensobs[0].value)
-        #KODE
-        self.motor_recommendation.update(self.priority * self.match_degree,motor_recommendation,halt_request)
+        if max(self.red_list < 500):
+            which_fifth = max(self.red_list)
+            degrees = 30-which_fifth*15
+            if abs(degrees) > 10:
+                motor_recommendation = ('F',300)
+            else:
+                motor_recommendation = ('S',degrees)
+        else:
+            motor_recommendation = ('S',60)
+
+        self.motor_recommendation.update(self.priority * self.match_degree,motor_recommendation)
