13b2e3d767f045418b672acd803943ba5e2c6007
Magnus Reier Rushfeldt <magnurr@stud.ntnu.no>
2018-11-15 08:30:27 +0100

    Camera testing

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from behavior import Behavior
 from sensob import Sensob
 from motob import Motob
 from motors import Motors
-from ir_sensob import IRSensob
-from follow_line_behavior import FollowLineBehavior
+from findRedSensob import FindRedSensob
+from findAndFollowRedBallBehavior import FindAndFollowRedBallBehaviour
 from arbitrator import Arbitrator
 from zumo_button import ZumoButton
 
@@ -15,6 +15,12 @@ def main():
     motor1 = Motors()
     motob1 = Motob(motor1)
     bbcon1.motobs = [motob1]
+
+    cam_sensor = FindRedSensob
+    find_ball = FindAndFollowRedBallBehaviour(bbcon1, 1)
+    find_ball.add_sensob(cam_sensor)
+    bbcon1.add_behavior(find_ball)
+    bbcon1.add_sensob(cam_sensor)
     """
     line_sensor = IRSensob()
     line_follow = FollowLineBehavior(bbcon1, 1)
@@ -23,15 +29,11 @@ def main():
     bbcon1.add_sensob(line_sensor)
     """
     zumo_button = ZumoButton()
-    #zumo_button.wait_for_press()
+    zumo_button.wait_for_press()
 
-    motob1.update((("T", 1),False))
-    """
+    #motob1.update((("T", 1),False))
     for i in range(0, 300):
         bbcon1.run_one_timestep()
         print("Iteration ", i)
     motor1.stop()
-
-
-"""
 
